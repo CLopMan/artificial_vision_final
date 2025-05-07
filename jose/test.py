0ef7f357975b9a5a95e0dd1ffa6cae03dadbaa5a
joseaverde <joseaverde@protonmail.com>
2025-05-07 15:27:46 +0200

    Minor update Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    En la rama main
    Tu rama está actualizada con 'origin/main'.
    
    Cambios a ser confirmados:
          nuevos archivos: cosas/kimi-no-na-wa-1.png
          nuevos archivos: cosas/kimi-no-na-wa-2.png
          nuevos archivos: cosas/pokemon-1.jpg
          nuevos archivos: cosas/titan-1.jpg
          nuevos archivos: cosas/trails-1.jpg
          modificados:     test.py

--- jose/test.py
+++ jose/test.py
@@ -1,21 +1,26 @@
 from neural_style import *
 import time
 import os
+import sys
 
-matplotlib.rcParams['figure.dpi'] = 300
+def main (style_path, steps, step):
+    matplotlib.rcParams['figure.dpi'] = 300
 
-root = os.path.join(os.path.dirname(os.getcwd()), "cesar", "neural_style", "images")
-content = Image(path=os.path.join(root, "photorealistic", "style", "tar3.png"))
-#style   = Image(path=os.path.join(root, "manga", "city-1.jpg"))
-style   = Image(path=os.path.join("cosas", "trails-1.jpg"))
+    root = os.path.join(os.path.dirname(os.getcwd()), "cesar", "neural_style", "images")
+    content = Image(path=os.path.join(root, "photorealistic", "style", "tar3.png"))
+    style = Image(path=style_path)
+    #style   = Image(path=os.path.join(root, "manga", "city-1.jpg"))
+    #style   = Image(path=os.path.join("cosas", "trails-1.jpg"))
 
-steps = 10
-step = 4
-output, matrix = apply_and_reverse_matrix(content, style, steps, step)
+    output, matrix = apply_and_reverse_matrix(content, style, steps, step)
 
-stamp = str(time.time()).replace(".", "-")
-show_matrix(matrix, steps, step)
-matplotlib.pyplot.savefig(os.path.join("results", "%s-matrix.png" % stamp))
-output.show()
-matplotlib.pyplot.savefig(os.path.join("results", "%s-output.png" % stamp))
-matplotlib.pyplot.show()
+    stamp = str(time.time()).replace(".", "-")
+    stamp += "-" + os.path.basename(style_path).split(".")[0]
+    show_matrix(matrix, steps, step)
+    matplotlib.pyplot.savefig(os.path.join("results", "%s-matrix.png" % stamp))
+    output.show()
+    matplotlib.pyplot.savefig(os.path.join("results", "%s-output.png" % stamp))
+    matplotlib.pyplot.show()
+
+if __name__ == "__main__":
+    main(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
